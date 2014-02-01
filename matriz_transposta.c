14f83720a8678ecf46b3a66cc70f00dcd3597bf1
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2014-02-01 12:06:54 -0800

    Software to calculate "transposta" of matriz
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- matriz_transposta.c
+++ matriz_transposta.c
@@ -1,26 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void criar_matriz ( int ***matriz, int linhas, int colunas) {
+void preenche_matriz ( int **matriz, int linhas, int colunas) {
 
-    int **matriz_resultante;
     int i, j;
-
-    matriz_resultante = malloc(sizeof (int ) * linhas);
-
-    for ( i = 0; i < linhas; i++)
-        matriz_resultante[i] = malloc (sizeof (int) * colunas);
 
     printf ("preencher matriz\n");
 
     for ( i = 0; i < linhas; i++) {
         for ( j = 0; j < colunas; j++) {
             printf ("Digite o valor de %d %d\n", i, j);
-            scanf ("%d", &matriz_resultante[i][j]);
+            scanf ("%d", &matriz[i][j]);
         }
     }
 
-    *matriz = matriz_resultante;
+}
+
+int **criar_matriz ( int linhas, int colunas) {
+
+    int **matriz;
+    int i, j;
+
+    matriz = malloc(sizeof (int ) * linhas);
+
+    for ( i = 0; i < linhas; i++)
+        matriz[i] = malloc (sizeof (int) * colunas);
+
+    return matriz;
+
 }
 
 void menu( int *escolha) {
@@ -33,7 +40,7 @@ void menu( int *escolha) {
     scanf ("%d", escolha);
 }
 
-void editar_matriz ( int ***matriz, int linhas, int colunas) {
+void editar_matriz ( int **matriz, int linhas, int colunas) {
 
     int l, c, novo_valor;
 
@@ -42,9 +49,30 @@ void editar_matriz ( int ***matriz, int linhas, int colunas) {
     scanf ("%d", &l);
     printf ("Digite a coluna para editar\n");
     scanf ("%d", &c);
+    printf ("Valor de %d %d é %d\n", l, c, matriz[l][c]);
     printf ("Digite o novo valor de %d %d\n", l, c);
-    scanf ("%d", matriz[l][c]);
+    scanf ("%d", &matriz[l][c]);
+
     printf ("Edição completa\n");
+}
+
+int **calcular_transposta ( int **matriz, int linhas, int colunas) {
+
+    int **matriz_transposta;
+    int i, j;
+
+    matriz_transposta = criar_matriz ( linhas, colunas);
+
+    printf ("tranformando matriz\n");
+
+    for ( i = 0; i < linhas; i++) {
+        for ( j = 0; j < colunas; j++) {
+            matriz_transposta[i][j] = matriz[j][i];
+            printf ("mt %d m %d", matriz_transposta[i][j], matriz[j][i]);
+        }
+    }
+
+    return matriz_transposta;
 }
 
 void imprime_matriz ( int **matriz, int linhas, int colunas) {
@@ -52,11 +80,11 @@ void imprime_matriz ( int **matriz, int linhas, int colunas) {
     int i, j;
 
     printf ("Imprimindo matriz\n");
-
+    printf ("Endereço matriz %p\n", matriz);
     for ( i = 0; i < linhas; i++) {
         printf ("|");
         for ( j = 0; j < colunas; j++) {
-            printf ("| %d |", matriz[i][j]);
+            printf ("| %d %p |", matriz[i][j], &matriz[i][j]);
         }
         printf ("|\n");
     }
@@ -77,14 +105,23 @@ main () {
     while (1) {
         menu(&escolha);
         switch (escolha) {
-            case 1: criar_matriz( &matriz, linhas, colunas);
+
+            case 1: matriz = criar_matriz( linhas, colunas);
+                    preenche_matriz ( matriz, linhas, colunas);
                     break;
-            case 2: editar_matriz( &matriz, linhas, colunas);
-                    break;
+
+            case 2: editar_matriz( matriz, linhas, colunas);
+                   break;
+
+            case 3: matriz = calcular_transposta( matriz, linhas, colunas);
+                   break;
+
             case 4: imprime_matriz( matriz, linhas, colunas);
                     break;
+
             default: printf ("'%d' não existe nas opções\n", escolha);
                     break;
+
         }
     }
 }
